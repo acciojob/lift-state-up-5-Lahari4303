c696ca70f16c35d22e474b78ada3aa17fb7dad29
Lahari4303 <glaharireddy24@gmail.com>
2026-07-12 15:06:53 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isLoggedIn , setIsLoggedIn] = useState(false)
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+
+        {!isLoggedIn ? (
+          <>
+          <label>Username:</label>
+          <input type="text"/>
+          <br/>
+          <label>Password:</label>
+          <input type="password"/>
+          <br/>
+          <button onClick={() => setIsLoggedIn(true)}>Login</button>
+          </>
+        ):(
+          <p>You are logged in!</p>
+        )
+      }
     </div>
   )
 }
